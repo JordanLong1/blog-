a919abf09e2efd893f92eb57a211fdf2b5e5ac86
Jordan <jordanleelong5@gmail.com>
2020-10-28 20:06:53 -0700

    wired up the store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'; 
 
+
+import App from './components/App'
+import reducers from './reducers'
+
+const store = createStore(reducers, applyMiddleware(thunk)) // this is how we hook up a middleware to a redux store\
+// <Provider makes the redux store available to any nested components that are wrapped in the connect() function
+// Redux thunk is a middleware that lets you call action creators that return a function instead of an object. 
+// ^^ the function recieves the stores dispatch method, which is then used to dispatch regular synchronous actions inside the body of the func once the asynch operations are completed
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    </Provider>, 
+    document.querySelector('#root')
+)
